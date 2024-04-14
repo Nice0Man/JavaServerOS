9c8f50274b8c732a08a109fa4613eaa68b445c4e
Eugene <e.krotov03@gmail.com>
2024-04-15 00:00:49 +0300

    create CSVApp

---
+++ src/main/java/server/editor/commands/CSVApp.java
@@ -0,0 +1,52 @@
+package server.editor.commands;
+
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class CSVApp {
+    List<CSVEditor> editors;
+    CSVEditor activeEditor;
+    CommandHistory history;
+
+    public void print(){
+        activeEditor.getDataList().forEach(row -> {
+            for (String value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        });
+    }
+
+    public void create(String[] strings){
+        executeCommand(new CreateCommand(this, activeEditor, strings));
+    }
+
+    public void read(){
+        executeCommand(new ReadCommand(this, activeEditor));
+    }
+
+    public void update(int rowIndex, String[] strings){
+        executeCommand(new UpdateCommand(this, activeEditor, rowIndex, strings ));
+    }
+
+    public void delete(int rowIndex){
+        executeCommand(new DeleteCommand(this, activeEditor, rowIndex));
+    }
+
+
+
+    private void executeCommand(Command command){
+        if (command.execute()){
+            history.push(command);
+        }
+    }
+
+    void undo(){
+        Command command = history.pop();
+        if (command != null){
+            command.undo();
+        }
+    }
+}
