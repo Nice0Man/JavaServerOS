b443be58701402c9a927c8104d284a5dc00e30de
Eugene <e.krotov03@gmail.com>
2024-04-15 00:00:00 +0300

    create CommandHistory

---
+++ src/main/java/server/editor/commands/CommandHistory.java
@@ -0,0 +1,20 @@
+package server.editor.commands;
+
+import java.util.Stack;
+
+
+public class CommandHistory {
+    private final Stack<Command> history;
+
+    public CommandHistory() {
+        history = new Stack<>();
+    }
+
+    public void push(Command command) {
+        history.push(command); // Добавляем команду в верхушку стека
+    }
+
+    public Command pop() {
+        return history.pop(); // Извлекаем и возвращаем команду с верхушки стека
+    }
+}
